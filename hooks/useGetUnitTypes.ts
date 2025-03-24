be9bde554f2bfb9eb8eca8de8bfaae0d0aaa5113
S00221754 <S00221754@atu.ie>
2025-03-24 17:12:48 +0000

    made adding unittypes strongly typed

---
+++ hooks/useGetUnitTypes.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { getUnitTypes } from "@/services/unit-type.Service";
+import { UnitType } from "@/types/unit-type";
+
+export const useGetUnitTypes = () => {
+  const [unitTypes, setUnitTypes] = useState<UnitType[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchUnitTypes = async () => {
+      try {
+        const data = await getUnitTypes();
+        setUnitTypes(data);
+      } catch (err: any) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUnitTypes();
+  }, []);
+
+  return {
+    unitTypes,
+    loading,
+    error,
+  };
+};
---
+++ types/unit-type.ts
@@ -0,0 +1,6 @@
+export type UnitType = {
+    id: string;
+    name: string;
+    // createdAt: string;
+    // updatedAt: string;
+  };
